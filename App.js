5fb6eb98f356e901089d582588168e0c100e6bea
C.T. Bell <charlestbell@gmail.com>
2023-07-14 14:38:34 -0400

    wip trying to get it working again

--- App.js
+++ App.js
@@ -9,11 +9,14 @@ import { Video, ResizeMode } from "expo-av";
 // const uri = `https://stream.mux.com/Tkgk5LC6an93zK1sDdaPkebJAdx0001E2A00kY1hXzDQ4g.m3u8?token=${jwt}`;
 
 // console.log("JWT", jwt);
-const apiKey = { apiKey: "YOUR API KEY HERE" };
+const apiKey = {
+  apiKey: "28vMw62aBjGyUvhwekzdpgjDIQLK9odRLSn062tCCIr",
+};
 
 export default function App() {
   const video = useRef(null);
   const [status, setStatus] = useState({});
+  const [token, setToken] = useState("");
 
   useEffect(() => {
     (async () => {
@@ -26,10 +29,11 @@ export default function App() {
         },
         body: JSON.stringify(apiKey),
       });
-      console.log("RESPONSE", JSON.stringify(await response.json(), null, 3));
+      // console.log("RESPONSE", JSON.stringify(await response.json(), null, 3));
       if (response.ok) {
         const parsedResponse = await response.json();
         console.log("PARSED RESPONSE", parsedResponse);
+        setToken(parsedResponse.access_token);
       } else {
         throw new Error();
       }
@@ -37,29 +41,24 @@ export default function App() {
     return;
   }, []);
 
-  // console.log('JWT', jwt)
+  console.log("TOKEN", token);
   return (
     <View style={styles.container}>
       <View style={{ backgroundColor: "yellow", width: 300, height: 300 }}>
-        {/* <Video */}
-        {/*   ref={video} */}
-        {/*   style={styles.video} */}
-        {/*   source={{ */}
-        {/*     uri, */}
-        {/*   }} */}
-        {/*   useNativeControls */}
-        {/*   resizeMode={ResizeMode.CONTAIN} */}
-        {/*   isLooping */}
-        {/*   onPlaybackStatusUpdate={(status) => setStatus(() => status)} */}
-        {/* />  */}
+        {/* <Video
+          ref={video}
+          style={styles.video}
+          source={{
+            uri,
+          }}
+          useNativeControls
+          resizeMode={ResizeMode.CONTAIN}
+          isLooping
+          onPlaybackStatusUpdate={(status) => setStatus(() => status)}
+        /> */}
       </View>
       <View style={{ backgroundColor: "yellow", width: 300, height: 300 }}>
-        <ApiVideoPlayer
-          videoId={{
-            id: "vi2DHbfHgu4pZLYKhuXOZXN1",
-            token: "f7af2a74-b3d1-49eb-9050-a909820bc903",
-          }}
-        />
+        <ApiVideoPlayer video={{ id: "vi7gDwXbUdULH27ha4DmeW6k", token }} />
       </View>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
